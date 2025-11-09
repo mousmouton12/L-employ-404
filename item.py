adfcf6efdabf0dacf6d900193a15a81b47368f85
Tryxio <bouras.amir10@gmail.com>
2025-11-09 12:39:24 +0100

    item.py

--- item.py
+++ item.py
@@ -4,10 +4,10 @@ import json
 data = {
     "bouteille_eau":{
         "id":"bouteille_eau",
-        "name":"bouteille d'eau",
+        "name":"Bouteille d'eau",
         "type":"consommable",
         "rarity":"commun",
-        "description":"Boisson transparente remplie de divers minéraux qui permet de restaurer 5 points de vie à celui qui la boit.",
+        "description":"Toujours aussi rafraîchisante. Permet de restaurer quelques points de vie à celui qui la boit.",
         "effects": {
             "heal":5
             
@@ -16,10 +16,10 @@ data = {
 
     "pomme":{
         "id":"pomme",
-        "name":"pomme",
+        "name":"Pomme",
         "type":"consommable",
         "rarity":"commun",
-        "description":"Fruit rouge juteux qui, du fait de rassasier et d’hydrater, restaure 6 points de vie à celui qui le mange.",
+        "description":"Il n'en existe plus beaucoup dans ce monde capitaliste. Restaure des points de vie à celui qui le mange.",
         "effects":{
             "heal":6
         }
@@ -27,10 +27,10 @@ data = {
 
     "cafe":{
         "id":"cafe",
-        "name":"cafe",
+        "name":"Café",
         "type":"consommable",
         "rarity":"commun",
-        "description":"Boisson dont une simple dose agit comme anesthésiant en soignant de 3 points de vie la personne qui la boit.",
+        "description":"Agit comme anesthésiant, soigne quelques points de vie de la personne qui la boit.",
         "effects":{
             "heal":3
         }
@@ -38,21 +38,21 @@ data = {
 
     "tasse":{
         "id":"tasse",
-        "name":"tasse",
-        "type":"arme",
+        "name":"Tasse : le meilleur employé !",
+        "type":"consommable",
         "rarity":"commun",
-        "description":"Jeter cet objet sur votre adversaire pour lui infliger 3 dégâts. Cependant, vous ne pourrez pas le réutiliser.",
+        "description":"Servait à la base à récompenser un employé. Jeter cet objet sur votre adversaire pour lui infliger des dégâts minimes. Cependant, vous ne pourrez pas le réutiliser.",
         "effects":{
-            "damage":3 #PROBLEME DUTILISATION UNIQUE
+            "damage":3
         }
     },
 
     "barre_chocolat":{
         "id":"barre_chocolat",
-        "name":"barre chocolatée",
+        "name":"Barre chocolatée",
         "type":"consommable",
         "rarity":"commun",
-        "description":"Petite sucrerie permettant de restaurer 4 points de vie à la personne qui la consomme.",
+        "description":"Petite sucrerie permettant de restaurer des points de vie à la personne qui la consomme.",
         "effects":{
             "heal":4
         }
@@ -60,10 +60,10 @@ data = {
 
     "stylo": {
         "id": "stylo",
-        "name": "stylo",
+        "name": "Stylo",
         "type": "arme",
         "rarity": "commun",
-        "description": "Un simple stylo, mais peut causer 2 dégâts à l'adversaire si il est utilisé avec précision.",
+        "description": "Un simple stylo, mais peut causer certains dégâts à l'adversaire si il est utilisé avec précision.",
         "effects": {
             "damage":2
         }
@@ -71,10 +71,10 @@ data = {
 
     "classeur": {
         "id": "classeur",
-        "name": "classeur",
+        "name": "Classeur",
         "type": "arme",
         "rarity": "commun",
-        "description": "Objet remplis de documents et fichiers lourds. Un simple lancer de l'objet sur l'ennemi lui cause 3 dégâts.",
+        "description": "Objet remplis de documents et fichiers lourds. Un simple lancer de l'objet sur l'ennemi lui cause des dégâts.",
         "effects": {
             "damage":3
         }
@@ -82,21 +82,21 @@ data = {
 
     "lunette":{
         "id":"lunette",
-        "name":"lunette",
+        "name":"Lunette",
         "type":"outil",
         "rarity":"commun",
-        "description":"Outil permettant de voir plus aisément les attaques ennemies. + 5% d'esquive",
+        "description":"Outil permettant de voir plus aisément les attaques ennemies et qui par conséquent augmente la chance de fuite.",
         "effects":{
-            "esquive":5 #ATTENTION, IL FAUDRA PRECISE QUE C'EST EN POURCENTAGE
+            "fuite":5
         }
     },
 
-    "sac":{
-        "id":"sac",
-        "name":"sac",
+    "chaise":{
+        "id":"chaise",
+        "name":"Chaise",
         "type":"arme",
         "rarity":"commun",
-        "description":"Objet qui permet d'infliger 2 dégâts à l'ennemi.",
+        "description":"Lancer de chaises, une compétition jadis très connue. Objet qui permet d'infliger quelques dégâts à l'ennemi.",
         "effects":{
             "damage":2
         }
@@ -104,10 +104,10 @@ data = {
     
     "chaussure":{
         "id":"chaussure",
-        "name":"chaussure",
+        "name":"Chaussure",
         "type":"arme",
         "rarity":"commun",
-        "description":"Jeter cet objet sur l'ennemi lui inflige 3 dégâts."
+        "description":"Les riches s'en servent pour marcher avec, les pauvres n'ont pas cette chance. Jeter cet objet sur l'ennemi lui inflige 3 dégâts.",
         "effects":{
             "damage":3
         }
@@ -115,7 +115,7 @@ data = {
 
     "agrafeuse":{
         "id":"agrafeuse",
-        "name":"agrafeuse",
+        "name":"Agrafeuse",
         "type":"arme",
         "rarity":"non commun",
         "description":"Outil permettant de faire très mal là où il faut.",
@@ -126,7 +126,7 @@ data = {
 
     "ciseau":{
         "id":"ciseau",
-        "name":"ciseau",
+        "name":"Ciseau",
         "type":"arme",
         "rarity":"non commun",
         "description":"Tranchant et rapide, l'ennemi n'aura pas le temps de réagir.",
@@ -137,21 +137,21 @@ data = {
 
     "produit_menager":{
         "id":"produit_menager",
-        "name":"produit_menager",
+        "name":"Produit ménager",
         "type":"arme",
         "rarity":"non commun",
-        "description":"Produit chimique irritant qui provoque des démangeaisons et des brûlures aux yeux de l’ennemi et lui inflige 7 dégâts.",
+        "description":"Produit chimique irritant qui provoque des démangeaisons et des brûlures aux yeux de l’ennemi et lui inflige des dégâts modestes.",
         "effects":{
             "damage":7
         }
     },
 
-    "latte_metallique":{
-        "id":"latte_metallique",
-        "name":"latte metallique",
+    "objet_metallique":{
+        "id":"objet_metallique",
+        "name":"Objet métallique",
         "type":"arme",
         "rarity":"non commun",
-        "description":"Objet à lancer sur l'adversaire et qui lui cause 5 dégâts.",
+        "description":"Utilisation initiale inconnue, mais c'était sûrement pour faire de l'argent.",
         "effects":{
             "damage":5
         }
@@ -159,10 +159,10 @@ data = {
 
     "perceuse":{
         "id":"perceuse",
-        "name":"perceuse",
+        "name":"Perceuse",
         "type":"arme",
         "rarity":"non commun",
-        "description":"Utile pour percer les défenses adverses."
+        "description":"Utile pour percer les défenses adverses.",
         "effects":{
             "damage":6
         }
@@ -170,11 +170,70 @@ data = {
 
     "couteau":{
         "id":"couteau",
-        "name":"couteau de cuisine",
+        "name":"Couteau de cuisine",
         "type":"arme",
         "rarity":"rare",
-        "description":"Arme permettant de trancher un ennemi.",
+        "description":"Une lame émoussée par le temps, mais dont le tranchant semble garder le souvenir de tout ce qu’elle a touché.",
+        "effects":{
+            "damage":15
+        }
+    },
+
+    "pistolet":{
+        "id":"pistolet",
+        "name":"Pistolet",
+        "type":"arme",
+        "rarity":"rare",
+        "description":"Le métal est froid, le poids rassurant. Une arme faite pour protéger...mais pas que.",
+        "effects":{
+            "damage":20
+        }
+    },
+
+    "matraque":{
+        "id":"matraque",
+        "name":"Matraque",
+        "type":"arme",
+        "rarity":"rare",
+        "description":"Solide et parfaitement équilibrée, elle inspire autant la sécurité que la crainte à celui qui la manie.",
         "effects":{
             "damage":10
         }
     },
+
+    "dossiers_rh":{
+        "id":"dossiers_rh",
+        "name":"Dossiers des Recherches Humaines",
+        "type":"outil",
+        "rarity":"epic",
+        "description":"Ces dossiers vous permettrons d'avoir un boost de +15% de chance de fuite.",
+        "effects":{
+            "fuite":15
+        }
+    },
+
+    "casque_vr":{
+        "id":"casque_vr",
+        "name":"Casque VR Premium Blindé",
+        "type":"outil",
+        "rarity":"epic",
+        "description":"Chef-d'œuvre de technologie et de puissance, ce casque VR blindé isole totalement son porteur du monde réel. Derrière sa visière fumée, la peur disparaît, remplacée par une lucidité glaciale. Rien ne semble pouvoir l’atteindre — ni balle, ni doute.",
+        "effects":{
+            "damage":20
+        }
+    },
+        
+    "potion_miracle":{
+        "id":"potion_miracle",
+        "name":"Potion Miracle",
+        "type":"consommable",
+        "rarity":"legendaire",
+        "description":"Potion permettant d'éliminer de façon instantané tous les ennemis à proximité. Une fois utilisé, vous ne pourrez pas réutilisez la même Potion Miracle.",
+        "effects":{
+            "damage":999
+        }
+    }
+}
+    
+with open("items.json", "w", encoding="utf-8") as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
