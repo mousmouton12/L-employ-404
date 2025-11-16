db39d9b45577a8b25cfb7ccf7ed8b1917dbec03d
Tryxio <bouras.amir10@gmail.com>
2025-11-16 16:16:36 +0100

    Update player.py

--- player.py
+++ player.py
@@ -6,6 +6,9 @@ def creer_joueur(nom):
         "PV_MAX":20,      #PV max initial (sans boosts)
         "ATK":8,          #Attaque initial (sans boosts)
         "Inventaire": [], #Liste qui va comprendre la place de stock qu'il y a dans l'inventaire.
-        "Competences":[]  #Liste qui va comprendre les différentes compétences du personnage.
+        "Competences": [],  #Liste qui va comprendre les différentes compétences du personnage.
+        "Bonus_fuite": 0,
+        "score": 0
     }
+
     return joueur
