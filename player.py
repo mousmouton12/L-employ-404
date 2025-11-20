1874048c34696d3cdb21c9e4b6001efff157b929
mousmouton12 <mousmouton12@gmail.com>
2025-11-20 16:39:42 +0100

    Add files via upload

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 def creer_joueur(nom):
 
     joueur = {
-        "nom":nom,     
+        "nom":nom,
         "PV":20,          #PV initial
         "PV_MAX":20,      #PV max initial (sans boosts)
         "ATK":8,          #Attaque initial (sans boosts)
@@ -14,3 +14,9 @@ def creer_joueur(nom):
 
     return joueur
 
+def ajouter_arme(joueur, arme):
+    joueur["Inventaire"].append(arme)
+    if "damage" in arme["effects"]:
+        bonus_degats = arme["effects"]["damage"]
+        joueur["ATK"] = joueur["ATK"] + bonus_degats
+        print("Vous avez équipé", arme["name"],"! Votre attaque augmente de", bonus_degats,"(Total :", joueur["ATK"],")")
