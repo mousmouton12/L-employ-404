06bf4602f37d1a218cbd5ad12e9b685fad09ce39
Tryxio <bouras.amir10@gmail.com>
2025-11-21 09:19:38 +0100

    Add files via upload

---
+++ corps.py
@@ -0,0 +1,8 @@
+#Intro Histoire
+print("""Introduction – L’employé 404 \nDans un monde dystopique où une méga industrie du nom de The Corporation a remplacé les gouvernements et règne dans tous les domaines sans partage ni justice, vous êtes Employé 404, une personne sans visage, sans avenir, parmis des milliers d'autres comme vous, travaillant dans cette méga entreprise. Dans cette méga structure, l'humain n'est que statistique et lorsque sa courbe de rentabilité chute, il disparaît... \nEn tant qu'Employé 404, vous êtes dans cette entreprise depuis des décennies mais vous le savez très bien, votre heure est proche. Vous devez faire un choix. The Corporation, le travail c'est la mort, la révolte, c'est l'espoir.""")
+
+#Espace
+print("\n")
+
+#Début
+print('\nTapez \"commencer"\ pour débuter votre révolte dans The Corporation.')
